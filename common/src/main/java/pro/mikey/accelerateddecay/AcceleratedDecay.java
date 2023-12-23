23c17315c4af7a1d50b42f0600a4e12fd04533d2
Kasualix <670067575@qq.com>
2023-12-23 21:53:23 +0800

    Backport to 1.16.5

--- common/src/main/java/pro/mikey/accelerateddecay/AcceleratedDecay.java
+++ common/src/main/java/pro/mikey/accelerateddecay/AcceleratedDecay.java
@@ -1,5 +1,6 @@
 package pro.mikey.accelerateddecay;
 
+import io.netty.util.internal.ConcurrentSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
@@ -13,22 +14,20 @@ import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.material.FluidState;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AcceleratedDecay {
     public static final String MOD_ID = "accelerateddecay";
 
-    private static final List<TimedDimBlockPos> timeBasedScanLocations = new ArrayList<>();
-
-    public static void init() {
-
-    }
+    private static final Set<TimedDimBlockPos> timeBasedScanLocations = new ConcurrentSet<>();
 
     public static void levelTickPostEvent(ServerLevel serverLevel) {
         if (timeBasedScanLocations.isEmpty()) {
@@ -36,38 +35,33 @@ public class AcceleratedDecay {
         }
 
         Instant now = Instant.now();
-        List<TimedDimBlockPos> locations = timeBasedScanLocations.stream()
-                .filter(e -> !e.checkAfter.isAfter(now) && serverLevel.dimension().equals(e.dim))
-                .toList();
 
-        if (locations.isEmpty()) {
-            return;
-        }
-
-        for (TimedDimBlockPos location : locations) {
-            if (location.player == null || !location.player.isAlive()) {
-                continue;
-            }
-
-            Set<BlockPos> yeetLeaves = seekLeaves(serverLevel, location.pos);
-
-            boolean isFirst = true;
-            for (BlockPos yeetLeaf : yeetLeaves) {
-                BlockState blockState = serverLevel.getBlockState(yeetLeaf);
-                if (!blockState.is(BlockTags.LEAVES)) {
+        for (TimedDimBlockPos location : timeBasedScanLocations) {
+            if (!location.checkAfter.isAfter(now) && serverLevel.dimension().equals(location.dim)) {
+                if (location.player == null || !location.player.isAlive()) {
                     continue;
                 }
 
-                // Allow events to block us
-                if (!CrossPlatform.emitBlockBrokeEvent(serverLevel, yeetLeaf, blockState, location.player)) {
-                    continue;
+                Set<BlockPos> yeetLeaves = seekLeaves(serverLevel, location.pos);
+
+                boolean isFirst = true;
+                for (BlockPos yeetLeaf : yeetLeaves) {
+                    BlockState blockState = serverLevel.getBlockState(yeetLeaf);
+                    if (!blockState.is(BlockTags.LEAVES)) {
+                        continue;
+                    }
+
+                    // Allow events to block us
+                    if (!CrossPlatform.emitBlockBrokeEvent(serverLevel, yeetLeaf, blockState, location.player)) {
+                        continue;
+                    }
+
+                    destroyBlockWithOptionalSoundAndParticles(serverLevel, yeetLeaf, true, 512, location.player, isFirst);
+                    isFirst = false;
                 }
 
-                destroyBlockWithOptionalSoundAndParticles(serverLevel, yeetLeaf, true, 512, location.player, isFirst);
-                isFirst = false;
+                timeBasedScanLocations.remove(location);
             }
-
-            timeBasedScanLocations.remove(location);
         }
     }
 
@@ -82,20 +76,21 @@ public class AcceleratedDecay {
 
     private static final BlockPos[] SCAN_LOCATIONS;
     static {
-        var box = new BoundingBox(BlockPos.ZERO).inflatedBy(1);
+        BoundingBox box = new BoundingBox(-1, -1, -1, 1, 1, 1);
         SCAN_LOCATIONS = BlockPos.betweenClosedStream(box).map(BlockPos::immutable).filter(e -> !e.equals(BlockPos.ZERO)).distinct().toArray(BlockPos[]::new);
     }
 
     private static Set<BlockPos> seekLeaves(Level level, BlockPos pos) {
         Set<BlockPos> validLocations = new HashSet<>();
         Set<BlockPos> walked = new HashSet<>();
-        Deque<BlockPos> nextToScan = new ArrayDeque<>(List.of(pos));
+        Deque<BlockPos> nextToScan = new ArrayDeque<>();
+        nextToScan.add(pos);
 
         while (!nextToScan.isEmpty()) {
-            var currentLocation = nextToScan.pop();
+            BlockPos currentLocation = nextToScan.pop();
             for (BlockPos offset : SCAN_LOCATIONS) {
                 BlockPos nextLocation = currentLocation.offset(offset);
-                var state = level.getBlockState(nextLocation);
+                BlockState state = level.getBlockState(nextLocation);
 
                 if (state.getBlock() instanceof LeavesBlock && !state.getValue(BlockStateProperties.PERSISTENT) && state.getValue(BlockStateProperties.DISTANCE) == 7 && validLocations.add(nextLocation)) {
                     if (walked.add(nextLocation)) {
@@ -120,20 +115,27 @@ public class AcceleratedDecay {
         }
 
         if (bl) {
-            BlockEntity blockEntity = blockState.hasBlockEntity() ? level.getBlockEntity(blockPos) : null;
+            BlockEntity blockEntity = blockState.getBlock().isEntityBlock() ? level.getBlockEntity(blockPos) : null;
             Block.dropResources(blockState, level, blockPos, blockEntity, player, ItemStack.EMPTY);
         }
 
         boolean bl2 = level.setBlock(blockPos, fluidState.createLegacyBlock(), 3, i);
         if (bl2) {
-            level.gameEvent(player, GameEvent.BLOCK_DESTROY, blockPos);
+            level.levelEvent(player, 2001, blockPos, Block.getId(blockState));
         }
     }
 
-    record TimedDimBlockPos(
-            Instant checkAfter,
-            BlockPos pos,
-            ResourceKey<Level> dim,
-            ServerPlayer player
-    ) {}
+    static final class TimedDimBlockPos {
+        private final Instant checkAfter;
+        private final BlockPos pos;
+        private final ResourceKey<Level> dim;
+        private final ServerPlayer player;
+
+        public TimedDimBlockPos(Instant checkAfter, BlockPos pos, ResourceKey<Level> dim, ServerPlayer player) {
+            this.checkAfter = checkAfter;
+            this.pos = pos;
+            this.dim = dim;
+            this.player = player;
+        }
+    }
 }
